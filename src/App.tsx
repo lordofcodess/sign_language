35f3f89c5b46baa1f5ae502e1217e86d48fb3457
Saintdannyyy <danieltesla746@gmail.com>
2025-11-17 12:03:20 +0000

    feat: add routes for Learn, Lesson, and PracticeComplete pages in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Translate from './pages/Translate';
-import Practice from './pages/Practice';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Translate from "./pages/Translate";
+import Learn from "./pages/Learn";
+import Lesson from "./pages/Lesson";
+import Practice from "./pages/Practice";
+import PracticeComplete from "./pages/PracticeComplete";
 
 function App() {
   return (
@@ -12,8 +15,11 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/lesson/:category" element={<Lesson />} />
             <Route path="/translate" element={<Translate />} />
             <Route path="/practice" element={<Practice />} />
+            <Route path="/practice-complete" element={<PracticeComplete />} />
           </Routes>
         </main>
       </div>
